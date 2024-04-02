46d5dc7cc9aa36f086f15eb79764879169621a2a
Pieter Deane <deanepieter@gmail.com>
2024-04-02 06:30:09 +0200

    Implemented LoginForm component with form validation and UI components

--- components/auth/login-form.tsx
+++ components/auth/login-form.tsx
@@ -1,16 +1,79 @@
+"use client"
 
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
 import { CardWrapper } from "@/components/auth/card-wrapper";
+import { LoginSchema } from "@/schemas";
+import { Input } from "@/components/ui/input";
+import * as z from "zod";
+import {
+    Form,
+    FormControl,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from "@/components/ui/form"
 
-const LoginForm = () => (
-    <CardWrapper
-        headerLabel="Welcome back"
-        backButtonLabel="Don't have an account ?"
-        backButtonHref="/auth/register"
-        showSocial
-    >
 
-        LoginForm
-    </CardWrapper>
-);
+const LoginForm = () => {
+    const form = useForm<z.infer<typeof LoginSchema>>({
+        resolver: zodResolver(LoginSchema),
+        defaultValues: {
+            email: "",
+            password: "",
+        }
+    });
+
+    return (
+
+        <CardWrapper
+            headerLabel="Welcome back"
+            backButtonLabel="Don't have an account ?"
+            backButtonHref="/auth/register"
+            showSocial
+        >
+            <Form {...form}>
+                <form
+                    onSubmit={form.handleSubmit(() => { })}
+                    className="space-y-6"
+                >
+                    <div className="space-y-4">
+                        <FormField
+                            control={form.control}
+                            name="email"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>Email</FormLabel>
+
+                                    <FormControl>
+
+                                        <Input
+                                            {...field}
+                                            type="email"
+                                            placeholder="john.doe@example.com"
+                                        />
+                                    </FormControl>
+                                    <FormMessage/>
+
+                                </FormItem>
+
+
+                            )}
+
+                        />
+                    </div>
+
+
+                </form>
+
+            </Form>
+
+        </CardWrapper>
+    )
+}
+
+
+
 
 export default LoginForm;
