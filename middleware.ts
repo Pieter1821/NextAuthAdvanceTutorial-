b71c892a78bdcc2b1fe3ea1219455de7b8ffabd2
Pieter Deane <deanepieter@gmail.com>
2024-04-07 08:14:09 +0200

    adding nextauth config to check user session using jwt strategy added protected routes

--- middleware.ts
+++ middleware.ts
@@ -1,15 +1,17 @@
-import { auth } from './auth';
+import NextAuth from 'next-auth';
+import authConfig from '@/auth.config';
 
+const { auth } = NextAuth(authConfig)
 
 export default auth((req) => {
-    const isLoggedIn = !!req.auth;
+  const isLoggedIn = !!req.auth;
 
-  console.log("ROUTE: " , req.nextUrl.pathname)
-  console.log("IS LOGGEDIN: ", isLoggedIn);
+  console.log('ROUTE: ', req.nextUrl.pathname);
+  console.log('IS LOGGEDIN: ', isLoggedIn);
 });
 
 // Optionally, don't invoke Middleware on some paths
 // Read more: https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
 export const config = {
-    matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+  matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
 };
