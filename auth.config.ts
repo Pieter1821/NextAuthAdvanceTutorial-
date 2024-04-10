696276003ef5eee356a6c91ec877b08e545eae5a
Pieter Deane <deanepieter@gmail.com>
2024-04-10 07:12:43 +0200

    adding callbacks to retrieve session tokens user details when logging in

--- auth.config.ts
+++ auth.config.ts
@@ -1,28 +1,44 @@
-import  bcrypt  from 'bcryptjs';
+import bcrypt from 'bcryptjs';
 
-import type { NextAuthConfig } from "next-auth"
-import credentials from "next-auth/providers/credentials"
+import type { NextAuthConfig } from 'next-auth';
+import credentials from 'next-auth/providers/credentials';
 
-import { LoginSchema } from "@/schemas"
-import { getUserByEmail } from "./data/user";
+import { LoginSchema } from '@/schemas';
+import { getUserByEmail } from './data/user';
 
-
+// Exporting the default configuration for NextAuth
 export default {
   providers: [
+    // Using the credentials provider for authentication
     credentials({
-      async authorize(credentials) { 
+      async authorize(credentials) {
+        // Validating the credentials using LoginSchema
         const validatedFields = LoginSchema.safeParse(credentials);
+
+        // If validation fails, return null
         if (!validatedFields.success) {
-          const { email , password} = validatedFields.data;
+          return null;
+        }
 
-          const user =  await getUserByEmail(email)
-          if (!user || !user.password) return null;
+        const { email, password } = validatedFields.data;
 
-          const passwordsMatch = await bcrypt.compare(password, user.password);
-          if (!passwordsMatch) return null;
+        // Getting user by email
+        const user = await getUserByEmail(email);
 
+        // If user not found or password not set, return null
+        if (!user || !user.password) {
+          return null;
+        }
+
+        // Comparing the password provided with the hashed password in the database
+        const passwordsMatch = await bcrypt.compare(password, user.password);
+
+        // If passwords  match, return user
+        if (passwordsMatch) {
           return user;
         }
-       }
-    })],
-} satisfies NextAuthConfig
+        return null;
+      },
+    }),
+  ],
+} as NextAuthConfig;
--- auth.ts
+++ auth.ts
@@ -9,7 +9,25 @@ const prisma = new PrismaClient();
 export const {
   handlers: { GET, POST },
   auth,
+  signIn,
+  signOut,
 } = NextAuth({
+  callbacks: {
+    async session({ session, token }) {
+      console.log({ sessionToken: token, session });
+
+      if (session.user) {
+        session.user.customField = token.customField;
+      }
+      return session;
+    },
+
+    async jwt({ token }) {
+      console.log({ token });
+      token.customField = 'test';
+      return token;
+    },
+  },
   adapter: PrismaAdapter(db),
   session: { strategy: 'jwt' },
   ...authConfig,
