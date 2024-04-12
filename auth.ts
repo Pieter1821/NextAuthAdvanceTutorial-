eaac908a1556588bfb7fcf48ac643ad01849c61a
Pieter Deane <deanepieter@gmail.com>
2024-04-12 06:29:41 +0200

    added role for user

--- auth.ts
+++ auth.ts
@@ -2,7 +2,9 @@ import NextAuth from 'next-auth';
 import { PrismaAdapter } from '@auth/prisma-adapter';
 import { PrismaClient } from '@prisma/client';
 import authConfig from '@/auth.config';
-import { db } from './lib/db';
+import { db } from '@/lib/db';
+import { getUserById } from '@/data/user';
+
 
 const prisma = new PrismaClient();
 
@@ -14,22 +16,29 @@ export const {
 } = NextAuth({
   callbacks: {
     async session({ session, token }) {
-      if(token.sub && session.user ) {
+      if (token.sub && session.user) {
         session.user.id = token.sub;
       }
 
-      if (session.user) {
-        session.user.customField = token.customField;
+      if(token.role && session.user) {
+        session.user.role = token.role
       }
+
       return session;
     },
 
     async jwt({ token }) {
-     
+      if (!token.sub) return token;
+      const existingUser = await getUserById(token.sub);
+      if (!existingUser) return token;
+
+      token.role = existingUser.role
+
       return token;
     },
   },
   adapter: PrismaAdapter(db),
   session: { strategy: 'jwt' },
+
   ...authConfig,
 });
